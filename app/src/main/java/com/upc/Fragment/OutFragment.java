1d2e863c3064ca84e105a46b3f67e24666549969
AccRight <my_romantic_story@163.com>
2017-04-20 10:33:40 +0800

    Android 6.0 Permission Manager

--- app/src/main/java/com/upc/Fragment/OutFragment.java
+++ app/src/main/java/com/upc/Fragment/OutFragment.java
@@ -135,95 +135,22 @@ public class OutFragment extends Fragment {
     /****************************************/
     ProgressDialog progressDialog;//查询等待框
     PermissionManager permissionManager;//动态权限请求
+    private static final int LOCATION_REQUEST = 1;//请求权限
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         user = BmobUser.getCurrentUser(User.class);
         View viewRoot = inflater.inflate(R.layout.outpage, container, false);
-        /*****************初始化百度地图********************/
-        mLocationClient = new LocationClient(getActivity().getApplicationContext());
-        mLocationClient.registerLocationListener(new BDLocationListener() {
-            @Override
-            public void onReceiveLocation(BDLocation bdLocation) {
-                StringBuffer sb = new StringBuffer(256);
-                sb.append("time : ");
-                sb.append(bdLocation.getTime());
-                sb.append("\nerror code : ");
-                sb.append(bdLocation.getLocType());
-                sb.append("\nlatitude : ");
-                sb.append(bdLocation.getLatitude());
-                sb.append("\nlontitude : ");
-                sb.append(bdLocation.getLongitude());
-                sb.append("\nradius : ");
-                sb.append(bdLocation.getRadius());
-                if (bdLocation.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
-                    sb.append("\nspeed : ");
-                    sb.append(bdLocation.getSpeed());// 单位：公里每小时
-                    sb.append("\nsatellite : ");
-                    sb.append(bdLocation.getSatelliteNumber());
-                    sb.append("\nheight : ");
-                    sb.append(bdLocation.getAltitude());// 单位：米
-                    sb.append("\ndirection : ");
-                    sb.append(bdLocation.getDirection());// 单位度
-                    sb.append("\naddr : ");
-                    sb.append(bdLocation.getAddrStr());
-                    sb.append("\ndescribe : ");
-                    sb.append("gps定位成功");
-
-                } else if (bdLocation.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
-                    sb.append("\naddr : ");
-                    sb.append(bdLocation.getAddrStr());
-                    //运营商信息
-                    sb.append("\noperationers : ");
-                    sb.append(bdLocation.getOperators());
-                    sb.append("\ndescribe : ");
-                    sb.append("网络定位成功");
-                } else if (bdLocation.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
-                    sb.append("\ndescribe : ");
-                    sb.append("离线定位成功，离线定位结果也是有效的");
-                } else if (bdLocation.getLocType() == BDLocation.TypeServerError) {
-                    sb.append("\ndescribe : ");
-                    sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
-                    nRecord.setLocationDetail("未知地点");
-                    nRecord.setLocation("未知");
-                    location.setText("无法获取地理位置，请打开相应权限");
-                } else if (bdLocation.getLocType() == BDLocation.TypeNetWorkException) {
-                    sb.append("\ndescribe : ");
-                    sb.append("网络不同导致定位失败，请检查网络是否通畅");
-                } else if (bdLocation.getLocType() == BDLocation.TypeCriteriaException) {
-                    sb.append("\ndescribe : ");
-                    sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
-                }
-                sb.append("\nlocationdescribe : ");
-                sb.append(bdLocation.getLocationDescribe());// 位置语义化信息
-                List<Poi> list = bdLocation.getPoiList();// POI数据
-                if (list != null) {
-                    sb.append("\npoilist size = : ");
-                    sb.append(list.size());
-                    for (Poi p : list) {
-                        sb.append("\npoi= : ");
-                        sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
-                    }
-                }
-                if (bdLocation.getCity()!=null&&bdLocation.getAddrStr()!=null)
-                {
-                    nRecord.setLocation(bdLocation.getCity());
-                    nRecord.setLocationDetail(bdLocation.getAddrStr());
-                    location.setText(bdLocation.getAddrStr());
-                    //Log.e("smile","获取的地点打印+++"+bdLocation.getCity()+bdLocation.getAddrStr());
-                    Log.e("baidu", "定位的结果++++++++" + sb.toString());
-                }
-            }
-        });
         /*******************请求权限**************************/
-        permissionManager = PermissionManager.with(this).addRequestCode(1)
-                .permissions(android.Manifest.permission.ACCESS_FINE_LOCATION).permissions(android.Manifest.permission.READ_PHONE_STATE)
-                .permissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE).setPermissionsListener(new PermissionListener() {
+        mLocationClient = new LocationClient(getActivity().getApplicationContext());
+        permissionManager = PermissionManager.with(this).addRequestCode(LOCATION_REQUEST)
+                .permissions(android.Manifest.permission.ACCESS_FINE_LOCATION,android.Manifest.permission.READ_PHONE_STATE,android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .setPermissionsListener(new PermissionListener() {
                     @Override
                     public void onGranted() {
                         //当权限被授予时调用
-                        //Toast.makeText(getActivity(), "请求权限成功",Toast.LENGTH_LONG).show();
+                        Log.e("smile","请求权限成功");
                         LocationClientOption option = new LocationClientOption();
                         option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
                         option.setOpenGps(true);//可选，默认false,设置是否使用gps
@@ -231,12 +158,85 @@ public class OutFragment extends Fragment {
                         option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
                         mLocationClient.setLocOption(option);
                         mLocationClient.start();
+                        mLocationClient.registerLocationListener(new BDLocationListener() {
+                            @Override
+                            public void onReceiveLocation(BDLocation bdLocation) {
+                                StringBuffer sb = new StringBuffer(256);
+                                sb.append("time : ");
+                                sb.append(bdLocation.getTime());
+                                sb.append("\nerror code : ");
+                                sb.append(bdLocation.getLocType());
+                                sb.append("\nlatitude : ");
+                                sb.append(bdLocation.getLatitude());
+                                sb.append("\nlontitude : ");
+                                sb.append(bdLocation.getLongitude());
+                                sb.append("\nradius : ");
+                                sb.append(bdLocation.getRadius());
+                                if (bdLocation.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                                    sb.append("\nspeed : ");
+                                    sb.append(bdLocation.getSpeed());// 单位：公里每小时
+                                    sb.append("\nsatellite : ");
+                                    sb.append(bdLocation.getSatelliteNumber());
+                                    sb.append("\nheight : ");
+                                    sb.append(bdLocation.getAltitude());// 单位：米
+                                    sb.append("\ndirection : ");
+                                    sb.append(bdLocation.getDirection());// 单位度
+                                    sb.append("\naddr : ");
+                                    sb.append(bdLocation.getAddrStr());
+                                    sb.append("\ndescribe : ");
+                                    sb.append("gps定位成功");
+
+                                } else if (bdLocation.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                                    sb.append("\naddr : ");
+                                    sb.append(bdLocation.getAddrStr());
+                                    //运营商信息
+                                    sb.append("\noperationers : ");
+                                    sb.append(bdLocation.getOperators());
+                                    sb.append("\ndescribe : ");
+                                    sb.append("网络定位成功");
+                                } else if (bdLocation.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                                    sb.append("\ndescribe : ");
+                                    sb.append("离线定位成功，离线定位结果也是有效的");
+                                } else if (bdLocation.getLocType() == BDLocation.TypeServerError) {
+                                    sb.append("\ndescribe : ");
+                                    sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+                                    nRecord.setLocationDetail("未知地点");
+                                    nRecord.setLocation("未知");
+                                    location.setText("无法获取地理位置，请打开相应权限");
+                                } else if (bdLocation.getLocType() == BDLocation.TypeNetWorkException) {
+                                    sb.append("\ndescribe : ");
+                                    sb.append("网络不同导致定位失败，请检查网络是否通畅");
+                                } else if (bdLocation.getLocType() == BDLocation.TypeCriteriaException) {
+                                    sb.append("\ndescribe : ");
+                                    sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+                                }
+                                sb.append("\nlocationdescribe : ");
+                                sb.append(bdLocation.getLocationDescribe());// 位置语义化信息
+                                List<Poi> list = bdLocation.getPoiList();// POI数据
+                                if (list != null) {
+                                    sb.append("\npoilist size = : ");
+                                    sb.append(list.size());
+                                    for (Poi p : list) {
+                                        sb.append("\npoi= : ");
+                                        sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
+                                    }
+                                }
+                                if (bdLocation.getCity()!=null&&bdLocation.getAddrStr()!=null)
+                                {
+                                    nRecord.setLocation(bdLocation.getCity());
+                                    nRecord.setLocationDetail(bdLocation.getAddrStr());
+                                    location.setText(bdLocation.getAddrStr());
+                                    //Log.e("smile","获取的地点打印+++"+bdLocation.getCity()+bdLocation.getAddrStr());
+                                    Log.e("baidu", "定位的结果++++++++" + sb.toString());
+                                }
+                            }
+                        });
                     }
 
                     @Override
                     public void onDenied() {
                         //用户拒绝该权限时调用
-                        Toast.makeText(getActivity(), "请求权限失败",Toast.LENGTH_LONG).show();
+                        Toast.makeText(getActivity(), "请求权限失败,打开权限已获取地理位置",Toast.LENGTH_LONG).show();
                     }
 
                     @Override
@@ -436,8 +436,93 @@ public class OutFragment extends Fragment {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
-            case 1:
+            case LOCATION_REQUEST:
+                //当权限被授予时调用
+                //Toast.makeText(getActivity(), "请求权限成功",Toast.LENGTH_LONG).show();
+                /*****************初始化百度地图********************/
+                Log.e("smile","运行到权限回调了");
                 permissionManager.onPermissionResult(permissions, grantResults);
+                //mLocationClient = new LocationClient(getActivity().getApplicationContext());
+                LocationClientOption option = new LocationClientOption();
+                option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+                option.setOpenGps(true);//可选，默认false,设置是否使用gps
+                option.setScanSpan(10000);//定位请求时间间隔
+                option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+                mLocationClient.setLocOption(option);
+                mLocationClient.start();
+                mLocationClient.registerLocationListener(new BDLocationListener() {
+                    @Override
+                    public void onReceiveLocation(BDLocation bdLocation) {
+                        StringBuffer sb = new StringBuffer(256);
+                        sb.append("time : ");
+                        sb.append(bdLocation.getTime());
+                        sb.append("\nerror code : ");
+                        sb.append(bdLocation.getLocType());
+                        sb.append("\nlatitude : ");
+                        sb.append(bdLocation.getLatitude());
+                        sb.append("\nlontitude : ");
+                        sb.append(bdLocation.getLongitude());
+                        sb.append("\nradius : ");
+                        sb.append(bdLocation.getRadius());
+                        if (bdLocation.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                            sb.append("\nspeed : ");
+                            sb.append(bdLocation.getSpeed());// 单位：公里每小时
+                            sb.append("\nsatellite : ");
+                            sb.append(bdLocation.getSatelliteNumber());
+                            sb.append("\nheight : ");
+                            sb.append(bdLocation.getAltitude());// 单位：米
+                            sb.append("\ndirection : ");
+                            sb.append(bdLocation.getDirection());// 单位度
+                            sb.append("\naddr : ");
+                            sb.append(bdLocation.getAddrStr());
+                            sb.append("\ndescribe : ");
+                            sb.append("gps定位成功");
+
+                        } else if (bdLocation.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                            sb.append("\naddr : ");
+                            sb.append(bdLocation.getAddrStr());
+                            //运营商信息
+                            sb.append("\noperationers : ");
+                            sb.append(bdLocation.getOperators());
+                            sb.append("\ndescribe : ");
+                            sb.append("网络定位成功");
+                        } else if (bdLocation.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                            sb.append("\ndescribe : ");
+                            sb.append("离线定位成功，离线定位结果也是有效的");
+                        } else if (bdLocation.getLocType() == BDLocation.TypeServerError) {
+                            sb.append("\ndescribe : ");
+                            sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+                            nRecord.setLocationDetail("未知地点");
+                            nRecord.setLocation("未知");
+                            location.setText("无法获取地理位置，请打开相应权限");
+                        } else if (bdLocation.getLocType() == BDLocation.TypeNetWorkException) {
+                            sb.append("\ndescribe : ");
+                            sb.append("网络不同导致定位失败，请检查网络是否通畅");
+                        } else if (bdLocation.getLocType() == BDLocation.TypeCriteriaException) {
+                            sb.append("\ndescribe : ");
+                            sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+                        }
+                        sb.append("\nlocationdescribe : ");
+                        sb.append(bdLocation.getLocationDescribe());// 位置语义化信息
+                        List<Poi> list = bdLocation.getPoiList();// POI数据
+                        if (list != null) {
+                            sb.append("\npoilist size = : ");
+                            sb.append(list.size());
+                            for (Poi p : list) {
+                                sb.append("\npoi= : ");
+                                sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
+                            }
+                        }
+                        if (bdLocation.getCity()!=null&&bdLocation.getAddrStr()!=null)
+                        {
+                            nRecord.setLocation(bdLocation.getCity());
+                            nRecord.setLocationDetail(bdLocation.getAddrStr());
+                            location.setText(bdLocation.getAddrStr());
+                            //Log.e("smile","获取的地点打印+++"+bdLocation.getCity()+bdLocation.getAddrStr());
+                            Log.e("baidu", "定位的结果++++++++" + sb.toString());
+                        }
+                    }
+                });
                 break;
         }
     }
